fb44a345be06b3eadbb9a2c5d3f89179415d5bab
Anh Lu <anhlucci@outlook.com>
2018-11-14 12:01:03 -0500

    yml

--- handler.js
+++ handler.js
@@ -1,6 +1,24 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+module.exports.s3_notification = async (event, context) => {
+  //extract S3 data from the event object
+  console.log('All records', event.Records);
+  console.log('S3 record', event.Records[0].s3);
+  const uploadData = event.Records.map(record =>{
+    return {
+      bucketName: record.s3.bucket.name,
+      file: record.s3.object.key,
+      fileSize: record.s3.object.size
+    }
+  })[0];
+
+  console.log('uploadData', uploadData);
+  // generate an email using the data above
+
+  // send the email
+
+  // send a response
+
   return {
     statusCode: 200,
     body: JSON.stringify({
